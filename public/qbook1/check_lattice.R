512cbc526f83b31eeb89964f851f12327a2165c3
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2023-11-19 23:29:07 +0700

    .

---
+++ public/qbook1/check_lattice.R
@@ -0,0 +1,12 @@
+library(readxl)
+doanh_thu_ban_hang <- read_excel("doanh_thu_ban_hang.xlsx")
+doanh_thu_ban_hang <- as.data.frame(doanh_thu_ban_hang)
+
+doanh_thu_ban_hang
+
+# Số liệu nhân viên theo giới tính và chức vụ ở các chi nhánh
+  
+
+library(lattice)
+
+
