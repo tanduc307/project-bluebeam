cb3b896cf9f0407ad4c6027c083d7ba5c1d56026
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2024-09-20 16:51:09 +0700

    .

--- public/codebase/growing/render.R
+++ public/codebase/growing/render.R
@@ -4,7 +4,7 @@ quarto:::quarto_render("growing.qmd")
 
 shell(cmd = "staticrypt growing.html -p r -t template.html --short")
 
-shell(cmd = "E:\\GITHUB\\project-bluebeam\\public\\codebase\\growing\\encrypted\\growing.html")
+# shell(cmd = "E:\\GITHUB\\project-bluebeam\\public\\codebase\\growing\\encrypted\\growing.html")
 
 unlink("growing.html")
 
