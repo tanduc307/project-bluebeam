0b3fdc81e62380a03ffa6d421343e66f38bc6d3c
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2024-09-20 17:02:17 +0700

    .

--- public/codebase/growing/render.R
+++ public/codebase/growing/render.R
@@ -2,9 +2,14 @@ library(quarto)
 
 quarto:::quarto_render("growing.qmd")
 
-shell(cmd = "staticrypt growing.html -p r -t template.html --short")
+shell(cmd = "staticrypt growing.html -p r -t template.html --short -d .")
+
+# --short bỏ đi thông báo password ngắn
+# -d . lưu lại ngay file ở directory
+# - t template.html lưu file theo template
+# -p r set password là r
 
 # shell(cmd = "E:\\GITHUB\\project-bluebeam\\public\\codebase\\growing\\encrypted\\growing.html")
 
-unlink("growing.html")
+# unlink("growing.html")
 
